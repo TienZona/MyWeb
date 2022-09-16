70b78bc676ddf6e3c17bd3a0f2c0eb94a310765c
TienZona <tienzona001@gmail.com>
2022-09-16 10:28:02 +0700

    Cai dat express chay thong bao xin chao

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const contactsRouter = require("./app/routes/contact.route");
+const app = express();
+const ApiError = require("./app/api-error");
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({message: "welcome to contact book application."});
+});
+
+app.use("/api/contacts", contactsRouter);
+
+
+// handle 404 response
+app.use((req, res, next) => {
+    return next(new ApiError(404, "resuorce not found"));
+})
+
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
+
+module.exports = app;
